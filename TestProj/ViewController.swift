13ab6d7416eb5c63ec10c82198446115538ed583
IvanMatchyn <winter.nes@gmail.com>
2021-12-08 21:04:27 +0200

    First app logic

--- TestProj/ViewController.swift
+++ TestProj/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var appLabel: UILabel!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        appLabel.isHidden = true
+        startButton.layer.cornerRadius = 10
+        appLabel.textColor = .systemBlue
+        startButton.setTitle("Show Text", for: .normal)
     }
 
-
+    @IBAction func startButtonHandler() {
+        appLabel.isHidden.toggle()
+        
+        if appLabel.isHidden {
+            startButton.setTitle("Show Text", for: .normal)
+        } else {
+            startButton.setTitle("Hide Text", for: .normal)
+        }
+    }
 }
 
